4ef9f0df6a72c16e18e49c5008edc6cdb7c655bc
Robert DeWilder <rob@worbli.io>
2018-10-25 12:13:15 -0400

    added eos titan to reserves

--- scripts/06-create-reserve-accounts.sh
+++ scripts/06-create-reserve-accounts.sh
@@ -9,3 +9,5 @@ worblim system newaccount eosio --transfer gravitypool1 EOS6HDHDrDWuWrCyrsLNgch4
 worblim system newaccount eosio --transfer chainrift EOS7RQ6fJTDzZTSoL6tmzXwfs9FLZTUPWDe6pNAhqqWHvcAxG7qdN --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer eosgermany EOS6TR2SD6XWVNLtutyjF8Zjr5YzNhnq2Ri2EN2S6H5dasVBrR9RE --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eostitan EOS7WjjoUg1tPfpkgYuuFFqAHU5r7p57dBT9KKjFsJgXBmSscQ5dw EOS7Pkpm3b9fqzHaQKfEZE8aa4HrdMPLJJoWYh6p4FLFJv2zV7Ffk --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
