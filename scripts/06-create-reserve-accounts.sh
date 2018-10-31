62cdf97ea202f605619bcec1b3a71e81347df303
Carlos Chen <18573318185@163.com>
2018-10-31 14:04:41 +0800

    Update producer name of gravitypool

--- scripts/06-create-reserve-accounts.sh
+++ scripts/06-create-reserve-accounts.sh
@@ -4,7 +4,7 @@ worblim system newaccount eosio --transfer eosnairobi EOS5ogxhfDgEe7rMtQ6qabhHGH
 
 worblim system newaccount eosio --transfer eosnation EOS8D5XVDVDCRk1uFrRXXqJdkxGWi5P1hXjYuVxQzDPZD6XGb4Jzv EOS73TkgKxoaeqzDW5hSNeHmmKcDe6JEL4rmsythEjfnwKQ7BJEZk --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
-worblim system newaccount eosio --transfer gravitypool1 EOS6HDHDrDWuWrCyrsLNgch4smwsXNrqM3cz3tz5wvvSsdTepvxHR --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
+worblim system newaccount eosio --transfer gravitypool EOS6HDHDrDWuWrCyrsLNgch4smwsXNrqM3cz3tz5wvvSsdTepvxHR --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer chainrift EOS7RQ6fJTDzZTSoL6tmzXwfs9FLZTUPWDe6pNAhqqWHvcAxG7qdN --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
